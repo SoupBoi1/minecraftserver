da7e431220a7d23bcf2fcc6261bc510a5d905419
SoupBoi1 <real.sudipta.halader@gmail.com>
2025-12-25 10:42:29 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+to start server ```sudo java -Xmx2G -jar fabric-server-mc.1.21.11-loader.0.18.3-launcher.1.1.0.jar nogui```
---
+++ save.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd /opt/minecraft_server
+echo "Initializing..."
+update_git=1
+yes=0
+#A time-consuming operation with a loop
+while : 
+do
+    #echo "Simulating.. Iteration "
+    if pgrep java > /dev/null ; then
+	  #  echo "yes"
+	    yes=1
+	    #update_git=1
+    else
+	    #echo "no"
+	    if [ $yes -eq 1 ];then
+	    	update_git=1
+	    fi
+    fi
+
+    if [ $update_git -eq 1 ];then
+
+	echo "AutoSave $(date +"%A, %b %d, %Y %H:%M:%S"	)"
+    	git add --all
+    	git commit -m "AutoSave $(date +"%A, %b %d, %Y %H:%M:%S")"
+    	git push 
+    	update_git=0
+    	yes=0
+    fi 
+    sleep 6h  #For each iteration, sleep for  6 hours
+done
+
+echo "Done"
+
